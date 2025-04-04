1a30bf5520c7e5dc7473b95caa62c243273812d4
corentin.t <corentin.tardivel@gmail.com>
2025-04-04 14:56:46 +0200

    ajout des config de base

---
+++ projet/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}', // Adaptez ce chemin selon la structure de votre projet
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('daisyui'),
+    ],
+  }
+  
